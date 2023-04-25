4e85bd6f8751cf6d73bf38364fcefbb1b8a9f3cc
Abhiram <a4abhiram125@gmail.com>
2023-04-25 16:30:29 +0530

    setup eslint to the backend

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const ShortUrl = require("./models/shortUrl");
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+
 const app = express();
 
-mongoose.connect("mongodb://localhost/urlShortener", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/shortUrls", async (req, res) => {
-    await ShortUrl.create({ full: req.body.fullUrl });
-    res.redirect("/");
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT || 5000);
-
-
