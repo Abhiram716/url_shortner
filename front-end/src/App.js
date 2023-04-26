087b6095e13f8614654dd07aaf1684c0c5129cce
Abhiram <a4abhiram125@gmail.com>
2023-04-26 21:34:04 +0530

    setup front-end with a framework

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,41 +1,62 @@
+// import axios from "axios";
+import { BaseProvider, LightTheme } from "baseui";
+import { Block } from "baseui/block";
+import { Button } from "baseui/button";
+import { Input } from "baseui/input";
+import { Table, SIZE } from "baseui/table-semantic";
+import { HeadingLarge } from "baseui/typography";
+import React from "react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
 import "./App.css";
-import axios from "axios";
-import React, { useState } from "react";
-
+const engine = new Styletron();
 function App() {
-  const [name, setName] = useState("nsbchjsbf");
-  let url = "http://127.0.0.1:5000/shortUrls";
+  // const [name, setName] = useState("nsbchjsbf");
+  // let url = "http://127.0.0.1:5000/shortUrls";
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    axios(url, {
-      method: "POST",
-      name:name
-    })
-      .then(response => response.data)
-      .catch(error => {
-        throw error;
-      });
-    console.log("clicked");
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   axios(url, {
+  //     method: "POST",
+  //     name:name
+  //   })
+  //     .then(response => response.data)
+  //     .catch(error => {
+  //       throw error;
+  //     });
+  //   console.log("clicked");
+  // };
   return (
-    <div className="App">
-      <form onSubmit={ handleSubmit }>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            className="form-input"
-            id="name"
-            value={ name }
-            onChange={ (e) => setName(e.target.value) }
-          />
-        </div>
-        <button type="submit">post</button>
-      </form>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Block
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Block>
+            <HeadingLarge>URL Shrinker</HeadingLarge>
+          </Block>
+          <Block>
+            <Input placeholder="www.example.com" />
+          </Block>
+          <Block marginTop={"50px"}>
+            <Button>post</Button>
+          </Block>
+          <Block marginTop={"scale1200"}>
+            <Table
+              columns={["FullUrl", "ShortUrl", "Clicks"]}
+              data={[
+                ["www.verylongURrrrrrl.com", "123456", "100 "],
+                ["www.verylongURrrrrrl.com", "123456", "100 "],
+              ]}
+              size={SIZE.compact}
+            />
+          </Block>
+        </Block>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
